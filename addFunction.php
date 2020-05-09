1a0944d87b7cd5d9a9ab19848302cda531e2801f
rg559 <55418010+rg559@users.noreply.github.com>
2020-05-09 19:39:36 -0400

    Update addFunction.php

--- addFunction.php
+++ addFunction.php
@@ -1,34 +1,20 @@
 <?php	
 	require "PDObject.php";
-	$status = "";
-		
-		if($_SERVER['REQUEST_METHOD'] == "POST") {
-    		insertTask($newID, $newMessage, $newCompleted, $newCreated, $newDue);
-    		
-			$newMessage = $_POST['Message'];
-			$newCompleted = $_POST['Completed'];
-			$newDue = $_POST['Due'];
-			
-		} else {
-			$insertStatement = "INSERT INTO todos(createddate, duedate, message, isdone) 
-			VALUES (CURRENT_TIMESTAMP, :Message, :Completed, :Due)";
-			
-			$stmt = $pdo->prepare($insertStatement);
-			$stmt->execute(['Message' => $newMessage, 'Completed' =>$newCompleted, 'Due' => $newDue]);
-			
-			$status = "Your data was sent.";
-			$newMessage = "";
-			$newCompleted = "";
-			$newDue = "";
-		}			
-	
-	//	if ($insertStatement->execute()) {
-	//	  echo "New record created successfully!";
-	//	  } else {
-	//	  echo "Unable to create record.";
-	//	  }
 
-	//	header("refresh:2; url=homework_table.php");
+
+function add_task($Task, $DueDate, $UserID) {
+    global $db;
+    $query = 'INSERT INTO to_do_items
+                 (Task, DueDate, UserID)
+              VALUES
+                 (:task, :duedate, :userid )';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':task', $Task);
+    $statement->bindValue(':duedate', $DueDate);
+    $statement->bindValue(':userid', $UserID);
+    $statement->execute();
+    $statement->closeCursor();
+}
 
 
 ?>
