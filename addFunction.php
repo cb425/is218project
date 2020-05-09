64565b7e13abc93886a99d8de4f70cd030e2fbfe
rg559 <55418010+rg559@users.noreply.github.com>
2020-05-09 19:48:59 -0400

    Update addFunction.php

--- addFunction.php
+++ addFunction.php
@@ -2,16 +2,15 @@
 	require "PDObject.php";
 
 
-function add_task($Task, $DueDate, $UserID) {
+function add_task($Message, $DueDate, $ID) {
     global $db;
-    $query = 'INSERT INTO to_do_items
-                 (Task, DueDate, UserID)
+    $query = 'INSERT INTO todos
+                 (message, duedate, id)
               VALUES
-                 (:task, :duedate, :userid )';
+                 (:message, :duedate, :userid )';
     $statement = $db->prepare($query);
-    $statement->bindValue(':task', $Task);
+    $statement->bindValue(':message', $Message);
     $statement->bindValue(':duedate', $DueDate);
-    $statement->bindValue(':userid', $UserID);
     $statement->execute();
     $statement->closeCursor();
 }
