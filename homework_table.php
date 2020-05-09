c49653fe486520d0b0f61fc279a452d09cdf2924
Ross <cb425@njit.edu>
2020-05-09 11:48:34 -0400

    homework table, shown tasks

--- homework_table.php
+++ homework_table.php
@@ -12,41 +12,37 @@ try {
     echo "Connected successfully";
     echo "<br>";
 
-  /*  $query = 'SELECT * FROM accounts where id < 6';
+    $query = 'SELECT * FROM todos';
     $statement = $conn ->prepare($query);
     $statement->execute();
     $accounts = $statement->fetchAll();
     $statement->closeCursor();
 
+    echo "Tasks: <br>";
     echo "<table width = 45%; border = 1px>
         <tr>
-            <th>id</th>
-            <th>email</th>
-            <th>fname</th>
-            <th>lname</th>
-
-            <th>birthday</th>
-            <th>gender</th>
-            <th>password</th>
-            
-            
+            <th>Task</th>
+            <th>owneremail</th>
+            <th>ownerid</th>
+            <th>createddate</th>
+            <th>duedate</th>
+            <th>message</th>
+            <th>isdone</th>           
         </tr>";
 
     foreach ($accounts as $result) {
         echo "<tr>
                     <td>".$result["id"]."</td>
-                    <td>".$result["email"]."</td>
-                    <td>".$result["fname"]."</td>
-                    <td>".$result["lname"]."</td>
-
-                    <td>".$result["birthday"]."</td>
-                    <td>".$result["gender"]."</td>
-                    <td>".$result["password"]."</td>
+                    <td>".$result["owneremail"]."</td>
+                    <td>".$result["ownerid"]."</td>
+                    <td>".$result["createddate"]."</td>
+                    <td>".$result["duedate"]."</td>
+                    <td>".$result["message"]."</td>
+                    <td>".$result["isdone"]."</td>
                </tr>";
     }
 
-    echo "<br>". 'There are '.count($accounts).' record(s) with a user ID less than 6 characters';
-    echo "<br><br>";*/
+    echo "<br><br>";
 }
 catch(PDOException $e)
 {
