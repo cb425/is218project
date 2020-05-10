fdfc57112482ec6acfb4c8b03762a3805fcaaea4
rg559 <55418010+rg559@users.noreply.github.com>
2020-05-10 11:49:18 -0400

    Create addUser.php

---
+++ addUser.php
@@ -0,0 +1,69 @@
+<?php
+if(!empty($_POST["add_record"])) {
+    require "PDObject.php";
+    $sql = "INSERT INTO accounts (email, fname, lname, phone, birthday, gender, passw) VALUES (:email, :fname, :lname, :phone, :birthday, :gender, :passw)";
+    $pdo_statement = $conn->prepare( $sql );
+
+    $result = $pdo_statement->execute( array(':email'=>$_POST['email'], ':fname'=>$_POST['fname'],':lname'=>$_POST['lname'], ':phone'=>$_POST['phone'], ':birthday'=>$_POST['birthday'], ':gender'=>$_POST['gender'], ':passw'=>$_POST['passw']) );
+    if (!empty($result) ){
+        header('location:homework_table.php');
+    }
+}
+?>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>index.html</title>
+<link href="main.css" rel="stylesheet" type="text/css">
+<script src="form.js"> </script>
+<link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+</head>
+<body>
+<div style="margin:20px 0px;text-align:right;"><a href="homework_table.php" class="button_link">Back to List</a></div>
+
+<h1>Sign up!</h1>
+<form action="homework_table.php" method="POST" name="signup" onsubmit="return validateForm()" onSubmit="return validateEmail()">
+        <label>Email: </label><br>
+        <input  type=email name="email" id="email" required/>
+        <br>
+        <br>
+        <label>First Name: </label><br>
+        <input type=text name="fname" id="fname" required />
+        <br>
+        <br>
+        <label> Last Name: </label>
+        <br>
+        <input type=text name="lname" id="lname" required/> 
+		<br>
+		<br>
+		<label>Phone: </label><br>
+        <input type="number" name="fname" id="fname" required />
+        <br>
+        <br>
+		<label>Birthday: </label><br>
+        <input type=text name="fname" id="fname" required />
+        <br>
+        <br>
+		<label>Gender: </label><br>
+        <input type="text" name="fname" id="fname" required />
+        <br>
+        <br>
+		<label>Password: </label><br>
+        <input type=password name="fname" id="fname" required />
+        <br>
+        <br>
+		<label>College: </label><br>
+        <input type=text name="fname" id="fname" required />
+        <br>
+        <br>
+		<label>Major: </label><br>
+        <input type=text name="fname" id="fname" required />
+        <br>
+        <br>
+	
+        <div class="demo-form-row">
+        <input name="add_record" type="submit" value="Add"">
+
+</form>
+</div>
+</body></html>
