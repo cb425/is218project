d2ff0156e2d31979c09df33b85fa4e3782917d16
Ross <cb425@njit.edu>
2020-05-09 21:00:49 -0400

    got insert to work lol

--- add.php
+++ add.php
@@ -1,31 +1,35 @@
-<!doctype html>
+<?php
+if(!empty($_POST["add_record"])) {
+    require "PDObject.php";
+    $sql = "INSERT INTO todos (message,duedate) VALUES ( :message, :duedate)";
+    $pdo_statement = $conn->prepare( $sql );
+
+    $result = $pdo_statement->execute( array( ':message'=>$_POST['message'],':duedate'=>$_POST['duedate']) );
+    if (!empty($result) ){
+        header('location:index.php');
+    }
+}
+?>
 <html>
 <head>
-<meta charset="UTF-8">
-<title>add.php</title>
-<link href="main.css" rel="stylesheet" type="text/css">
-<script src="form.js"> </script>
-<link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+    <title>Add task</title>
 </head>
-
 <body>
-	
-<?php
-	require 'addFunction.php';
-?>
-	
-		<form action="homework_table.php" method="post">
-		  <div class="container">
-			
-			  <label for="task"> Task: </label>
-			  <input type=text name="message" placeholder="Enter a description:" id="message" required> <br><br>
-			
-			  <label for="duedate"> Due Date: </label>
-				<input type=datetime-local name="duedate" id="duedate" required/> <br><br>
-			
-			  <button type="submit">Add to List!</button>
-		  </div>
-		</form>
-	
-</body>
-</html>	
+<div style="margin:20px 0px;text-align:right;"><a href="homework_table.php" class="button_link">Back to List</a></div>
+
+<h1>Add task</h1>
+<form action="" method="POST">
+
+    <div class="demo-form-row">
+        <label>Description: </label><br>
+        <textarea name="message" class="demo-form-field" rows="5" required ></textarea>
+    </div>
+
+    <label for="duedate"> Due Date: </label>
+    <input type=datetime-local name="duedate" id="duedate" required/> <br><br>
+
+    <div class="demo-form-row">
+        <input name="add_record" type="submit" value="Add" class="demo-form-submit">
+
+</form>
+</div>
