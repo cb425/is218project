74563a255bbb482648ec928a971680ab43e3da42
Ross <cb425@njit.edu>
2020-05-10 12:56:43 -0400

    fixed hw table

--- homework_table.php
+++ homework_table.php
@@ -6,9 +6,8 @@
     $statement->execute();
     $accounts = $statement->fetchAll();
     $statement->closeCursor();
+/*
 
-    echo "Tasks: <br>";
-    echo "(refresh page to see changes) <br>";
     echo "<table width = 45%; border = 1px>
         <tr>
             <th>ID</th>
@@ -27,7 +26,7 @@
         echo "<tr>
                     <td>".$result["id"]."</td>
                     <td>".$result["title"]."</td>
-                    <td>".$result["message"]."</td> 
+                    <td>".$result["message"]."</td>
                     <td>".$result["createddate"]."</td>
                     <td>".$result["duedate"]."</td>";
 
@@ -93,9 +92,6 @@ foreach ($accounts as $result) {
     }
 }
 
-
-
-
     //COMPLETED TASKS
 
 echo "<table width = 45%; border = 1px>
@@ -129,12 +125,146 @@ foreach ($accounts as $result) {
                </tr>";
     }
 }
-
-
-
-
+*/
 
 ?>
+
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>index.html</title>
+    <link href="main.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+</head>
+
+<body>
+<h1>Task list</h1>
+<p>refresh page to see changes</p>
+
+<!-- ENTIRE TABLE -->
+<table width = 45%; border = 1px>
+    <tr>
+        <th>ID</th>
+        <th>Title</th>
+        <th>Description</th>
+        <th>createddate</th>
+        <th>duedate</th>
+        <th>Completed?</th>
+    </tr>
+<?php
+foreach ($accounts as $result) {
+    $task = $result["id"];
+    echo "<tr>
+                    <td>" . $result["id"] . "</td>
+                    <td>" . $result["title"] . "</td>
+                    <td>" . $result["message"] . "</td> 
+                    <td>" . $result["createddate"] . "</td>
+                    <td>" . $result["duedate"] . "</td>";
+
+    if ($result["isdone"] == NULL) {
+        echo "<td>
+                    No
+                    </td>";
+
+    }
+
+    if ($result["isdone"] == "on") {
+        echo "<td>
+                    Yes
+                    </td>";
+    }
+
+    echo "<td>
+                   <a href=\"delete.php?id=$task\" class=\"edit\">Delete</a></td>  
+                   <td>
+                   <a href=\"edit.php?id=$task\" class=\"edit\">Edit</a></td>
+                    
+               </tr>";
+}
+?>
+
+
+    <!-- INCOMPLETE TASK -->
+    <table width = 45%; border = 1px>
+        <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Description</th>
+            <th>createddate</th>
+            <th>duedate</th>
+            <th>Completed?</th>
+
+
+
+        </tr>
+
+        <?php
+        foreach ($accounts as $result) {
+            $task = $result["id"];
+            if($result["isdone"] == NULL) {
+                echo "<tr>
+                    <td>" . $result["id"] . "</td>
+                    <td>" . $result["title"] . "</td>
+                    <td>" . $result["message"] . "</td> 
+                    <td>" . $result["createddate"] . "</td>
+                    <td>" . $result["duedate"] . "</td>
+                    <td>No</td>
+                    <td>
+                    <a href=\"delete.php?id=$task\" class=\"edit\">Delete</a></td>  
+                    <td>
+                    <a href=\"edit.php?id=$task\" class=\"edit\">Edit</a></td>
+                    
+               </tr>";
+            }
+        }
+
+        ?>
+        <p>Incomplete Tasks</p>
+        <!-- COMPLETE TASK -->
+
+        <table width = 45%; border = 1px>
+            <tr>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Description</th>
+                <th>createddate</th>
+                <th>duedate</th>
+                <th>Completed?</th>
+
+
+
+            </tr>
+
+            <?php
+            foreach ($accounts as $result) {
+                $task = $result["id"];
+                if($result["isdone"] == "on") {
+                    echo "<tr>
+                    <td>" . $result["id"] . "</td>
+                    <td>" . $result["title"] . "</td>
+                    <td>" . $result["message"] . "</td> 
+                    <td>" . $result["createddate"] . "</td>
+                    <td>" . $result["duedate"] . "</td>
+                    <td>Yes</td>
+                    <td>
+                    <a href=\"delete.php?id=$task\" class=\"edit\">Delete</a></td>  
+                    <td>
+                    <a href=\"edit.php?id=$task\" class=\"edit\">Edit</a></td>
+                    
+               </tr>";
+                }
+            }
+
+            ?>
+            <p>Complete Tasks</p>
+
+
+
+
+
+</body>
+</html>
 
 
 
