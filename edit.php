4ee56e654c3178cc6a7725666abf3ff59c9c1cea
Ross <cb425@njit.edu>
2020-05-10 00:35:24 -0400

    edit default values in

--- edit.php
+++ edit.php
@@ -1,8 +1,11 @@
 <?php
 
+
 if (isset($_GET['id'])) {
     if(!empty($_POST["save"])) {
-        require "PDObject.php";
+        require_once "PDObject.php";
+
+
         $sql = "UPDATE todos SET title =:title, message = :message, duedate= :duedate  WHERE id=". $_GET['id'];
         $pdo_statement = $conn->prepare( $sql );
 
@@ -11,39 +14,49 @@ if (isset($_GET['id'])) {
         header('location:homework_table.php');
     }
     }
+
 }
+
 
 else {
     exit('No ID specified!');
 }
 
+
+require "PDObject.php";
+$query = "SELECT * FROM todos WHERE id=".$_GET['id'];
+$statement = $conn ->prepare($query);
+$statement->execute();
+$theData = $statement->fetch(PDO::FETCH_ASSOC);
+
 ?>
 <html>
 <head>
-    <title>Edit Task <?php> $_GET['id']</?></title>
+    <title>Edit Task
+    </title>
     <script src="form.js"> </script>
 </head>
 <body>
 <div style="margin:20px 0px;text-align:right;"><a href="homework_table.php" class="button_link">Back to List</a></div>
 
-<h1>Add task</h1>
+<h1>Edit Task <?php echo $_GET['id']?></h1>
 <form action="" method="POST" name="addTask" onSubmit="return validateDescription()">
     <label>Title: </label><br>
-    <input  type="text" name="title" id="title" required/>
+    <input  type="text" name="title" id="title" value="<?php echo $theData['title'];?>" required/>
     <br>
     <br>
     <label>Description: </label><br>
-    <textarea name="message" id="message" rows="5" required></textarea>
+    <input  type="text" name="message" id="message" rows="5" value="<?php echo $theData['message']?>" required/>
 
     <br>
     <br>
     <label for="duedate"> Due Date: </label>
     <br>
-    <input type=datetime-local name="duedate" id="duedate" required/> <br><br>
+    <input type=datetime-local name="duedate" id="duedate" value="<?=date('Y-m-d\TH:i', strtotime($theData['duedate']))?>" required/> <br><br>
 
     <div class="demo-form-row">
         <input name="save" type="submit" value="Add"">
 
 </form>
 </div>
-</body></html>
+</body></html>*/
